807d444449b80a15a54172ecbeb54740418dd741
Edson Ayllon <EAYLLON1@GMAIL.COM>
2019-07-31 03:32:54 +0000

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # react context with web3
- 
+
+## 1 | Description
+
+A React app with a single Web3 instance held globally using React Context.
+
+![Screenshot](./Screenshop_4.jpg)
+
+## 2 | Getting Started
+
+### 2.1 Install
+
+Install dependencies for the React Client.
+
+```
+cd client
+yarn || npm install
+```
+
+
+### 2.2 Run
+
+Run the client. In `./client`:
+
+```
+yarn start || npm run start
+```
--- client/src/App.js
+++ client/src/App.js
@@ -35,17 +35,7 @@ function ChildElement() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br/ >
         <button onClick={handleLogin} type="button">Login with Metamask</button>
         <Child2 />
       </header>
